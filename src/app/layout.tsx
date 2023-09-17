f50576fbf2b766084dd4f749c4069b18ee648e8b
Jorge Chavez <53054594+jorgearmandooc@users.noreply.github.com>
2023-09-16 20:54:42 -0600

    feat: datatable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet"/>
         <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet"/>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css" rel="stylesheet"/>
-        
+        <link href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" rel="stylesheet"/>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
--- src/app/proveedores/page.tsx
+++ src/app/proveedores/page.tsx
@@ -1,31 +1,89 @@
+'use client';
+
 import Image from 'next/image'
 import HeaderNavComponent from '@/components/header';
+import Link from 'next/link';
+import Head from 'next/head';
+import Script from 'next/script';
+import { useEffect} from 'react';
+
+import DataTable from 'datatables.net-dt';
 
 export default function Proveedores() {
-  return (<>
-    <HeaderNavComponent></HeaderNavComponent>
+  
+  function deleteProveedor(table:any, idProveedor: string) {
+    fetch(`http://localhost:8080/proveedores/${idProveedor}`, {
+      method: "DELETE"
+    })
+    .then(() => {
+      table.ajax.reload();
+    });
+  }
 
+  useEffect(() => {
+    let table = new DataTable('#dataTable', {
+      ajax: 'http://127.0.0.1:8080/proveedores',
+      processing: true,
+      serverSide: true,
+      order:[[1,"asc"]],
+      language: {
+        url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json',
+      },
+      columns:[
+        {
+          title: "ID",
+          data: 'idProveedor'
+        },
+        {
+          title: "NOMBRE",
+          data: 'nombre'
+        },
+        {
+          title: "RAZÓN SOCIAL",
+          data: 'razonSocial'
+        },
+        {
+          title: "DIRECCIÓN",
+          data: 'direccion'
+        },
+        {
+          data: null,
+          render: (data:any, type:any, row:any, settings: any) => { //data, type, row 
+            let html = '';
+            html += '<button class="btn btn-danger btn-datatable mr-2" data-idProveedor="' + data.idProveedor +'"><i class="fa fa-times" aria-hidden="true"></i></button>';
+            return html;
+          }
+        }
+      ],
+      drawCallback: function(e:any) {
+        let btnsDatatable = table.$(".btn-datatable").each( function(index: number, btn: any){
+          btn.addEventListener('click', () => {
+            deleteProveedor(table, table.$(btn).data('idproveedor'));
+          } );
+        });
+      }
+    });
+  })
+  
+  return (<>
+    <Head>
+    <link href="cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" rel="stylesheet"/>
+    </Head>
+
+    <HeaderNavComponent></HeaderNavComponent>
+  
     <main>
       <div className="container pt-4">
         <section className="mb-4">
           <div className="card">
             <div className="card-header py-3">
-              <h5 className="mb-0 text-center"><strong>E-Commerce GAPSI</strong></h5>
+              <h5 className="mb-0 text-center"><strong>Proveedores</strong></h5>
             </div>
             <div className="card-body">
 
-                <div className="text-center">
-                  <Image
-                  src="/logo.png"
-                  height="200"
-                  width="200"
-                  alt=""
-                  loading="lazy"
-                  className="img-thumbnail"
-                  />
-                </div>
-                <h2 className="text-center">Bienvenido Candidato 1</h2>
-                <p className="text-center">Versión: 0.0.1</p>
+              <Link href="/proveedores/create" className="btn float-end btn-primary mr-2 mb-4"><i className="fa fa-plus" aria-hidden="true"></i> Crear Proveedor</Link>
+
+              <table id="dataTable" className="table datatable"></table>
 
             </div>
           </div>
